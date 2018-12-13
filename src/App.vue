20deb1919e27747cdc888b7bfc81ed5ed7f847c9
s17001 <s17001@std.it-college.ac.jp>
2018-12-13 20:39:39 +0900

    よーいスタート

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <img src="../Pokemon-DB/img/025Pikachu.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+
+
+  import pokeJSON from '../Pokemon-DB/pokedex'
+
+  console.log(pokeJSON);
+
+  export default {
+    name: 'App',
+    data () {
+      return {
+        name: "",
+        type: "",
+        denNumber: ""
+      }
+    },
+    methods: {
+      }
+
+  }
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
