a151d49a64704cf856d24563af65bb5aa1387878
s17001 <s17001@std.it-college.ac.jp>
2018-12-14 15:08:42 +0900

    根本を書き換えた

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,83 @@
 <template>
   <div id="app">
-    <img src="../Pokemon-DB/img/025Pikachu.png">
-    <input type="number" v-model="dexNumber" style="background-color: ghostwhite">
-    <button v-on:click="setPokemon()" style="background-color: black"></button>
+    <img src="./assets/logo.png">
+    <h1>{{name}}</h1>
+
+    <input type="text" name="yourarea" autocomplete="on" list="tokyo"　v-model="name">
+    <datalist id="tokyo">
+
+      <div v-for="pokemon in pokeList">
+        <option :value="pokemon" ></option>
+      </div>
+
+    </datalist>
+
+    <br>
+
+    getpoke<button type="button" v-on:click="getPokemon()" style="background-color: black;">取得</button>
+    setpokeid<button type="button" v-on:click="setPokemonId()"></button>
     <p>{{dexNumber}}</p>
-    <p>{{pokemonData['ename']}}</p>
-    <p>{{pokemonData['base']}}</p>
-    <p>{{pokemonData}}</p>
-    <router-view/>
   </div>
 </template>
 
 <script>
 
+  import axios from 'axios'
+  import poke from 'pokemon'
 
-  import pokeJSON from '../Pokemon-DB/pokedex'
-  import pokeType from '../Pokemon-DB/types'
+  console.log(poke.getName(1,'ja'))
 
-  console.log(pokeJSON[0]);
-  console.log(pokeType);
+  const pokeList = poke.all('ja')
 
   export default {
     name: 'App',
     data () {
       return {
-        name: "",
-        type: "",
-        dexNumber: "",
-        pokemonData: ""
+        name: "ピカチュウ",
+        type: [],
+        dexNumber: "25",
+        pokemonData: "",
+        url: "https://pokeapi.co/api/v2/pokemon/",
+        pokeList: pokeList
       }
     },
-    methods: {
-      setPokemon () {
-        this.pokemonData = pokeJSON[this.dexNumber -1]
+    methods : {
+      getPokemon () {
+        axios.get(this.url+this.dexNumber+"/")
+          .then((res)=>{console.log(res["data"])})
+        // .then((json)=>{console.log(json)})
       },
-      }
+      setPokemonId () {
+        let flag = false
 
+        for (let i of pokeList){
+          if (this.name == i) {
+            this.dexNumber = poke.getId(this.name, 'ja')
+            flag = false
+            break;
+          } else {
+            flag = true
+          }
+
+        }
+
+        if (flag) {
+          this.name = "存在しないポケモンです"
+        }
+
+
+      }
+    }
   }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
