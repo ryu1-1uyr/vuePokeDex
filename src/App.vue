a3ffbd2fa86e262955953d80c22a0fde133a1b3d
s17001 <s17001@std.it-college.ac.jp>
2018-12-14 17:04:50 +0900

    見た目まで実装

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,15 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img :src="img" v-on:mouseover="over()" v-on:mouseleave="leave()" v-on:click="colored()">
     <h1>{{name}}</h1>
-
-    <input type="text" name="yourarea" autocomplete="on" list="tokyo"　v-model="name">
-    <datalist id="tokyo">
-
-      <div v-for="pokemon in pokeList">
-        <option :value="pokemon" ></option>
-      </div>
-
-    </datalist>
-
-    <br>
-
-    getpoke<button type="button" v-on:click="getPokemon()" style="background-color: black;">取得</button>
-    setpokeid<button type="button" v-on:click="setPokemonId()"></button>
+    <input type="text" v-model="name" placeholder="ポケモンの名前〜〜">
+    <button type="button" v-on:click="setPokemonId()">ＢＯＴＡＮＮ</button>
+    <p>{{ type }}</p>
+    <p>{{ type2 }}</p>
+    <p>おもさ : {{weight}}</p>
+    <p>たかさ : {{height}}</p>
     <p>{{dexNumber}}</p>
+    <h1 style="color:red;">{{ error }}</h1>
   </div>
 </template>
 
@@ -33,23 +26,52 @@
     name: 'App',
     data () {
       return {
-        name: "ピカチュウ",
-        type: [],
-        dexNumber: "25",
+        name: "",
+        type: "",
+        type2: "",
+        error: "",
+        weight: "",
+        height: "",
+        img: "",
+        front: "",
+        back: "",
+        front_shiny: "",
+        back_shiny: "",
+        dexNumber: "",
         pokemonData: "",
-        url: "https://pokeapi.co/api/v2/pokemon/",
-        pokeList: pokeList
+        url: "https://pokeapi.co/api/v2/pokemon/"
       }
     },
     methods : {
       getPokemon () {
         axios.get(this.url+this.dexNumber+"/")
-          .then((res)=>{console.log(res["data"])})
+          .then((res)=>{
+            console.log(res["data"])
+
+            this.type = res["data"]["types"][0]["type"]["name"]
+
+            if (res["data"]["types"][1]) {
+              this.type2 = res["data"]["types"][1]["type"]["name"]
+            } else {
+              console.log("unchi")
+              this.type2 = ""
+            }
+
+            this.weight = res["data"]["weight"]
+            this.height = res["data"]["height"]
+
+            this.front = res["data"]["sprites"]["front_default"]
+            this.back  = res["data"]["sprites"]["back_default"]
+
+            this.front_shiny = res["data"]["sprites"]["front_shiny"]
+            this.back_shiny  = res["data"]["sprites"]["back_shiny"]
+
+            this.img = this.front
+          })
         // .then((json)=>{console.log(json)})
       },
       setPokemonId () {
         let flag = false
-
         for (let i of pokeList){
           if (this.name == i) {
             this.dexNumber = poke.getId(this.name, 'ja')
@@ -58,15 +80,26 @@
           } else {
             flag = true
           }
-
         }
-
         if (flag) {
-          this.name = "存在しないポケモンです"
+          this.error = "存在しないポケモンです"
+        } else {
+          this.error = null
+          this.getPokemon()
         }
-
-
+      },
+      over() {
+        this.img = this.back
+      },
+      leave() {
+        this.img = this.front
+      },
+      colored() {
+        this.img = this.front_shiny
       }
+
+
+
     }
   }
 </script>
